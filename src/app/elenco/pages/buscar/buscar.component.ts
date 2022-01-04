5502ec53d0117262b794411a45b0baed5625f817
moises-llanos <moisesllanoswas@gmail.com>
2022-01-04 16:49:58 -0500

    COMMIT: Change color

--- src/app/elenco/pages/buscar/buscar.component.ts
+++ src/app/elenco/pages/buscar/buscar.component.ts
@@ -18,10 +18,7 @@ export class BuscarComponent implements OnInit {
   constructor(private service: ElencoService, 
               private router: Router) { }
 
-  ngOnInit(): void {
-
-    
-  }
+  ngOnInit(): void {}
 
 
   sugerenciasPersonajes(){
